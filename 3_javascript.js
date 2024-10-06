8ef205c7616cc96d382664ad9fd07db24d4f1ed7
muhali <031317495@ecampus.ut.ac.id>
2024-10-06 23:57:11 +0700

    materi pemanasan js

---
+++ 3_javascript.js
@@ -0,0 +1,6 @@
+console.log('ini eksperiment');
+// tips : taruh script js di akhir/ plg bawah html
+// seolah olah itu membuat web load quickly
+const monthRent = 450;
+const yearRent = monthRent * 12;
+console.log(yearRent);
