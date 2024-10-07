2520fe8538228139826bc4273507c43dcc43f4cc
muhali <031317495@ecampus.ut.ac.id>
2024-10-07 20:06:46 +0700

    malah random outputnya pake padStart

--- 3_exercise.js
+++ 3_exercise.js
@@ -1,11 +1,17 @@
-let freq = 10;
+let freq = 100;
 let char = '😵‍💫';
 
-let word = ''
-for (let i=0;i<freq;i++){
-    word+=char;
-    console.log(word);
-}
-console.log(word);// smart move from pak brian
+let word = '';
+// for (let i=0;i<freq;i++){
+//     word+=char;
+//     console.log(word);
+// }
+// console.log(word);
+
+// smart move from pak brian
 // dia meng-concatenate string ke emot
 // agar jadi 1 baris aja
+
+// short cheat pake fungsi padstart
+console.log(''.padStart(freq,char));
+// tapi hasilnya malah random bjir emot nya
