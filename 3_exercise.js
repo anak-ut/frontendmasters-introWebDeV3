4cad04bcdd4c6fc95950ed7e92636217c5171c67
muhali <031317495@ecampus.ut.ac.id>
2024-10-07 19:50:25 +0700

    alasan di concatenate ke string word

--- 3_exercise.js
+++ 3_exercise.js
@@ -6,4 +6,6 @@ for (let i=0;i<freq;i++){
     word+=char;
     console.log(word);
 }
-console.log(word);
+console.log(word);// smart move from pak brian
+// dia meng-concatenate string ke emot
+// agar jadi 1 baris aja
