bcf885a4b92fc83573204c3999f1c6bd6819c1bf
muhali <031317495@ecampus.ut.ac.id>
2024-10-13 22:30:36 +0700

    mempengaruhi redSquare dari warna 'red' ke 'biru'

--- 4_puttingAllTogether.js
+++ 4_puttingAllTogether.js
@@ -4,3 +4,7 @@
 // DOM berisi banyak objek dan method yg dapat dipanggil pakai js utk mempengaruhi html css
 
 // mari kita coba dengan 4_dom.html, 4_dom.css
+
+let redSquare = document.querySelector('.red-square');
+
+redSquare.style.backgroundColor = 'blue';
