2f6dcdc7230bd9efaeccc0e78eabb1e3f4b2d1c7
muhali <031317495@ecampus.ut.ac.id>
2024-10-13 22:19:33 +0700

    set up file html, css dan js utk subkategori DOM

---
+++ 4_puttingAllTogether.js
@@ -0,0 +1,6 @@
+// -- DOM
+
+// cara JS berinteraksi dengan html dan css = DOM
+// DOM berisi banyak objek dan method yg dapat dipanggil pakai js utk mempengaruhi html css
+
+// mari kita coba dengan 4_dom.html, 4_dom.css
