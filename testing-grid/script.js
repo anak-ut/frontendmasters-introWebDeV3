3203e599434f41a136e4723a6d654a72eff1aa97
hengker-tzy <031317495@ecampus.ut.ac.id>
2024-10-19 20:05:54 +0700

    click tombol sudah berfungsi. tinggal melakukan operasi math

---
+++ testing-grid/script.js
@@ -0,0 +1,5 @@
+let frameBtn = document.querySelector('.frame');
+let display = document.querySelector('.display');
+frameBtn.addEventListener('click', function(event){
+    display.innerText = `${event.target.innerText}`
+})
