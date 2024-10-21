9727b06ddfcfc253abb2f06bda848a362cd9a7cd
hengker-tzy <031317495@ecampus.ut.ac.id>
2024-10-21 19:25:17 +0700

    perbaiki nama class button

--- testing-grid/script.js
+++ testing-grid/script.js
@@ -1,5 +1,6 @@
 let frameBtn = document.querySelector('.frame');
 let display = document.querySelector('.display');
 frameBtn.addEventListener('click', function(event){
-    display.innerText = `${event.target.innerText}`
+    display.innerText = `${event.target.innerText}`;
+    
 })
