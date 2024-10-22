3eebb7f3566bebd4bfef21fbac670c8bf8e4e76c
hengker-tzy <031317495@ecampus.ut.ac.id>
2024-10-22 16:38:15 +0700

    menampilkan output klik

--- testing-grid/script.js
+++ testing-grid/script.js
@@ -1,13 +1,13 @@
 let frameBtn = document.querySelector('.frame');
 let display = document.querySelector('.display');
 frameBtn.addEventListener('click', function(event){
-    // display.innerText = `${event.target.innerText}`;
     let key= event.target;
     let action=key.dataset.action;
     if (!action){
         console.log('angka ter-klik');
     } else if (action == 'add' || action =='subtract'
         || action == 'multiply' || action == 'divide' || action == 'divide') {
-        console.log('oprator ter-klik');
-    }
+            console.log('oprator ter-klik');
+        }
+        display.innerText = `${event.target.innerText}`;
 });
