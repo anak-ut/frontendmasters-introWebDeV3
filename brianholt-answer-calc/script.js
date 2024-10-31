61baafe88bff1cb1eb088e4f0bac2174be0c69e4
muhali <031317495@ecampus.ut.ac.id>
2024-10-31 17:49:49 +0700

    mari kita follow step by step thinking mas brian holt

--- brianholt-answer-calc/script.js
+++ brianholt-answer-calc/script.js
@@ -2,4 +2,39 @@ let runningTotal =0;
 let buffer = '0';
 let previousOperator;
 const screen =  document.querySelector('.screen');
+const button = document.querySelector('.calc-buttons');
 
+function init(){
+    button.addEventListener('click', event => buttonClick(event.target.innerText))
+}
+
+function buttonClick(value){
+    console.log(value);
+    if (isNaN(parseInt(value))){
+        handleSymbol(value);
+        console.log('ini simbol');
+    } else {
+        handleNumber(value);
+        console.log('ini number');
+    }
+    rerender();
+}
+
+function handleNumber(number){
+    if (buffer === '0'){
+        buffer = number;
+    } else {
+        buffer += number;
+    }
+    console.log(buffer);
+}
+
+function handleSymbol(symbol){
+
+}
+
+function rerender(){
+    screen.innerText = buffer;
+}
+
+init();
