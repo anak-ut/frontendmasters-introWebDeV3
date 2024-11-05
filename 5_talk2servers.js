f59ec101d354ff94111a095381d92aabbdfbec58
muhali <031317495@ecampus.ut.ac.id>
2024-11-05 19:33:56 +0700

    belajar consume API

--- 5_talk2servers.js
+++ 5_talk2servers.js
@@ -7,6 +7,7 @@ tetapi server biasanya tidak mengirim dalam bentuk objek tetapi berbentuk string
 
 */
 
+/*
 const responseFromServer = `{"name": "Luna","age": 10,"breed": "Havanese","location": {"city":"Seattle","state": "WA"}}`;
 
 console.log(responseFromServer); // output string
@@ -25,3 +26,36 @@ console.log(parseResponse.location);
 const dataClient = {nama: 'jokonto77', usia: 69};
 const dataToSend = JSON.stringify(dataClient);
 console.log(dataToSend);
+*/
+
+/*
+AJAX
+
+API : adalah url yang dapat membuat request.
+API seperti website tetapi hanya utk machine.
+
+metodnya dapat merekues ke komputer utk mendapatkan informasi
+*/
+
+ const DOG_URL = "https://dog.ceo/api/breeds/image/random";
+ const dogDisplay = document.getElementById('dog-target');
+
+ function addDog(){
+    const asu = fetch(DOG_URL);
+    console.log(asu);
+    asu.then(function (response){
+        const processPromise = response.text();
+        return processPromise;
+    })
+    .then(function (proccedResponse){
+        const dogObject = JSON.parse(proccedResponse);
+        const img = document.createElement('img');
+        img.src = dogObject.message;
+        img.alt = 'anjenk';
+        dogDisplay.innerHTML = '';
+        console.log(dogDisplay);
+        dogDisplay.append(img);
+    });
+ }
+
+ document.getElementById('dog-btn').addEventListener('click', addDog);
