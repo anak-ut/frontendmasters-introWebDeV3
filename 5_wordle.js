a5d8555ee0e2536b1a7ca3fb324a34ff9ebd6244
muhali <031317495@ecampus.ut.ac.id>
2024-11-25 22:04:53 +0700

    using inspect-network

--- 5_wordle.js
+++ 5_wordle.js
@@ -10,7 +10,7 @@ async function init() {
   let currentGuess = "";
   let done = false;
   let isLoading = true;
-
+  
   // nab the word of the day
   const res = await fetch("https://words.dev-apis.com/word-of-the-day");
   const { word: wordRes } = await res.json();
@@ -19,7 +19,7 @@ async function init() {
   const wordParts = word.split("");
   isLoading = false;
   setLoading(isLoading);
-
+  
   // user adds a letter to the current guess
   function addLetter(letter) {
     if (currentGuess.length < ANSWER_LENGTH) {
@@ -39,8 +39,6 @@ async function init() {
       return;
     }
     
-    currentRow ++;
-    currentGuess = "";
     // check the API to see if it's a valid word
     // skip this step if you're not checking for valid words
     isLoading = true;
@@ -52,17 +50,17 @@ async function init() {
     const { validWord } = await res.json();
     isLoading = false;
     setLoading(isLoading);
-
+    
     // not valid, mark the word as invalid and return
     if (!validWord) {
       markInvalidWord();
       return;
     }
-
+    
     const guessParts = currentGuess.split("");
     const map = makeMap(wordParts); // map dari kata yang ditebak
     let allRight = true;
-
+    
     // first pass just finds correct letters so we can mark those as
     // correct first
     for (let i = 0; i < ANSWER_LENGTH; i++) {
@@ -72,7 +70,7 @@ async function init() {
         map[guessParts[i]]--;
       }
     }
-
+    
     // second pass finds close and wrong letters
     // we use the map to make sure we mark the correct amount of
     // close letters
@@ -90,7 +88,10 @@ async function init() {
         letters[currentRow * ANSWER_LENGTH + i].classList.add("wrong");
       }
     }
-
+    
+    currentRow++;
+    currentGuess = "";
+    
     if (allRight) {
       // win
       alert("you win");
@@ -102,20 +103,20 @@ async function init() {
       done = true;
     }
   }
-
+  
   // user hits backspace, if the the length of the string is 0 then do
   // nothing
   function backspace() {
     currentGuess = currentGuess.substring(0, currentGuess.length - 1);
     letters[currentRow * ANSWER_LENGTH + currentGuess.length].innerText = "";
   }
-
+  
   // let the user know that their guess wasn't a real word
   // skip this if you're not doing guess validation
   function markInvalidWord() {
     for (let i = 0; i < ANSWER_LENGTH; i++) {
       letters[currentRow * ANSWER_LENGTH + i].classList.remove("invalid");
-
+      
       // long enough for the browser to repaint without the "invalid class" so we can then add it again
       setTimeout(
         () => letters[currentRow * ANSWER_LENGTH + i].classList.add("invalid"),
@@ -123,7 +124,7 @@ async function init() {
       );
     }
   }
-
+  
   // listening for event keys and routing to the right function
   // we listen on keydown so we can catch Enter and Backspace
   document.addEventListener("keydown", function handleKeyPress(event) {
@@ -131,9 +132,9 @@ async function init() {
       // do nothing;
       return;
     }
-
+    
     const action = event.key;
-    console.log(action);
+    console.log(`this ${action}`);
 
     if (action === "Enter") {
       commit();
